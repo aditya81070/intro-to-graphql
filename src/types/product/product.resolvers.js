fca53835a588bfaeb230646a771c12ba292b3054
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-10-01 22:41:41 +0530

    complete lesson3 exercise

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,49 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const products = () => {
+  return Product.find({}).exec()
+}
+
+const product = (_, { id }) => {
+  return Product.findById(id).exec()
+}
+
+const newProduct = async (_, { input }, ctx) => {
+  const product = { ...input, createdBy: ctx.user._id }
+  const res = await Product.create(product)
+  return res
+}
+
+const updateProduct = async (_, { id, input }) => {
+  return await Product.findByIdAndUpdate(id, input, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = async (_, { id }) => {
+  return await Product.findByIdAndRemove(id)
+    .lean()
+    .exec()
+}
+
+const createdBy = async product => {
+  return await User.findById(product.createdBy)
+    .lean()
+    .exec()
+}
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy
   }
 }
